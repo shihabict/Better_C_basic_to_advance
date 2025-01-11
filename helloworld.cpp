cfcdf069b3a9e7484d0c4ece5d3c7bb8e1b3c61e
shihabict <shihabuddin.ict@gmail.com>
2025-01-11 12:45:51 -0600

    Added variables and datatypes

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main(){
+
+    // This is single line comment !
+    /*
+        This is 
+        multiline comment!
+    */    
     std:: cout << "Hello Students!" << std::endl;
     std:: cout << "Welcome to UAH!" << std::endl;
 
---
+++ practice-00-variables-datatypes.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main(){
+    int x; // variable decliration
+    x = 5; // assinment
+    double y = 10.6;
+    std:: cout << x << '\n';
+    std:: cout << y << '\n';
+
+
+    // String or Single character
+    char grad = 'A';
+    char first_name = 'B';
+
+    std:: cout << first_name <<":" << grad << "\n"; 
+
+    // Boolean
+    bool student = false;
+
+    std:: cout <<"Mr." << first_name <<" is student :" << student << "\n";
+
+
+    // String
+
+    std::string firstName = "Jhon";
+    std::string secondName = "Doe";
+
+    std:: cout << "Good Morning MR." << firstName << " " << secondName;
+
+
+    return 0;
+}
